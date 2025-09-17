6cd915fc838e91f8d3d3b85ad0627171d4ea9bc2
Martin <martinbf@hotmail.no>
2025-09-17 20:22:36 +0200

    Initial commit

---
+++ Client.py
@@ -0,0 +1,52 @@
+import socket
+
+
+class Client:
+    """Represents a tcp client with ipv4 ip in a computer network with a single socket"""
+
+    def __init__(self, ipType, transportProtocol):
+        self.createSocket(ipType, transportProtocol)
+
+    def createSocket(self, ipType, transportProtocol):
+        """Creates a Socket object with a ip protocol(IP Address) and a transport protocol(Transport layer)"""
+        self.socket = socket.socket(ipType, transportProtocol)
+
+    def connectToServer(self, host, port=1238):
+        """Connects the client to a server using a server host address and a port number"""
+        self.socket.connect((host, port))
+
+    def closeSocket(self):
+        """Closes the socket"""
+        self.socket.close()
+
+    def sendData(self, data):
+        """Sends data to the Server"""
+        self.socket.send(data.encode())
+
+    def receiveData(self, bufferSize=4096):
+        """Receives data from the Server"""
+        return self.socket.recv(bufferSize)
+
+    def getSocket(self):
+        """Returns the socket object"""
+        return self.socket
+
+def main():
+    ipType = socket.AF_INET
+    transportProtocol = socket.SOCK_STREAM
+    client = Client(ipType, transportProtocol)
+    host = "127.0.0.1"
+    port = 65432
+    message = ("Hello World")
+    try:
+        client.connectToServer(host, port)
+        client.sendData(message)
+        response = client.receiveData()
+        print("Recieved data from server")
+        print(response.decode())
+    except Exception as e:
+        print("Error connecting and sending to server")
+    finally:
+        client.closeSocket()
+if __name__ == "__main__":
+    main()
---
+++ Server.py
@@ -0,0 +1,71 @@
+import socket
+
+class Server:
+    """Represents a tcp server with ipv4 ip that handles clients for the network with a single socket"""
+
+    def __init__(self, host):
+        """Creates a Server with a host ip address"""
+        self.host = host
+        self.createSocket()
+        self.clients = []
+
+    def createSocket(self):
+        """Creates the socket for the server"""
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def bindSocket(self, port=1238):
+        """Binds the socket to the given port"""
+        self.socket.bind((self.host, port))
+
+    def listenForConnection(self):
+        """Listens for a connection"""
+        self.socket.listen()
+        print(f"Server listening on {self.host}")
+
+    def acceptConnection(self):
+        """Accepts a connection and returns the connection and connection address"""
+        conn, addr = self.socket.accept()
+        print(f"Connection accepted from {self.host}")
+        self.clients.append((conn, addr))
+        return conn, addr
+
+    def closeConnection(self, conn):
+        """Closes the connection and removes it from the clientlist"""
+        for i in self.clients:
+            if i[0] == conn:
+                i[0].close()
+                self.clients.remove(i)
+
+    def handleClient(self, conn):
+        """Handles a client connection by sending a the message back to the client.
+        Needs to be implemented by subclasses"""
+        try:
+            dataHasNotBeenRecieved = False
+            while not dataHasNotBeenRecieved:
+                data = conn.recv(1024)
+                if not data:
+                    dataHasNotBeenRecieved = True
+                conn.sendall(data)
+        except Exception as e:
+            print("Error with client communication")
+
+    def closeSocket(self):
+        """Closes the socket"""
+        self.socket.close()
+
+def main():
+    host = "127.0.0.1"
+    port = 65432
+    server = Server(host)
+    try:
+        server.bindSocket(port)
+        server.listenForConnection()
+        conn, addr = server.acceptConnection()
+        server.handleClient(conn)
+    except Exception as e:
+        print("Server error", e)
+    finally:
+        server.closeSocket()
+
+if __name__ == "__main__":
+    main()
